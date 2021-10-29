797b6e7a6e5aa5ceea1b82c5f1f705c4cfc2af0a
Twist <lsa1669778762@gmail.com>
2021-10-29 17:00:01 +0800

    npmignore

--- script/publish.sh
+++ script/publish.sh
@@ -1,5 +1,8 @@
 #bin/bush
 
+# 查看登录信息
+npm whoami
+
 # 登录
 npm adduser
 
@@ -8,3 +11,6 @@ npm adduser
 
 # 推送 公共发布
 npm publish . --access public
+
+#撤回已发布的版本
+# npm unpublish -f <package name>@<package version>
